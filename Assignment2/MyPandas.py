31e5f1b9f4045cd4741ea3e74d4d262d4d06d682
aamangulati <agulati@stevens.edu>
2017-02-09 17:34:07 -0500

    Assignment 2
    MyPandas

--- Assignment2/MyPandas.py
+++ Assignment2/MyPandas.py
@@ -1,5 +1,8 @@
 import OrderedDict
 import csv
+import OrderedDict
+import parse
+import datetime,time
 
 from collections import OrderedDict
 
@@ -107,6 +110,43 @@ class DataFrame(object):
             return self
         else:
             raise Exception('incorrect number')
+#task3
+    def min(self,col_name):
+        nums ,is_time= self.transform_type(col_name)
+        result = min(nums)
+        return datetime.datetime.fromtimestamp(result) if is_time else result
 
+    def max(self,col_name):
+        nums, is_time = self.transform_type(col_name)
+        result = max(nums)
+        return datetime.datetime.fromtimestamp(result) if is_time else result
+
+    def median(self,col_name):
+        nums, is_time = self.transform_type(col_name)
+        nums = sorted(nums)
+        center = int(len(nums) / 2)
+        if len(nums) % 2 == 0:
+            result = sum(nums[center - 1:center + 1]) / 2.0
+            return datetime.datetime.fromtimestamp(result) if is_time else result
+        else:
+            result = nums[center]
+            return datetime.datetime.fromtimestamp(result) if is_time else result
+
+    def mean(self,col_name):
+        nums, is_time = self.transform_type(col_name)
+        result = sum(nums)/len(nums)
+        return datetime.datetime.fromtimestamp(rslt) if is_time else result
+
+    
+    def sum(self,col_name):
+        nums,is_time = self.transform_type(col_name)
+        return sum(nums)
+
+    def std(self,col_name):
+        nums ,is_time = self.transform_type(col_name)
+        mean = sum(nums)/len(nums)
+        return (sum([(num-mean)**2 for num in nums])/len(nums))**0.5
+
+    
         
 
